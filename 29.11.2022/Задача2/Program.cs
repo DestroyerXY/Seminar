5f3b605dfb067adb00ba6275a3edd880560ed7f0
«Ramil <Ramil1995@bk.ru>
2022-11-30 22:42:20 +0300

    Задача2

---
+++ 29.11.2022/Задача2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Write number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write number");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine(a);
+else
+    Console.WriteLine(b);
