4b8394461c0b3871b8203c97642afc2be8e947af
«Ramil <Ramil1995@bk.ru>
2022-12-15 19:32:12 +0300

    Задача38

---
+++ Домашнее задание к семинару 13.12.2022/Задача 38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и 
+//  минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(-100, 101);
+}
+
+void ReleaseArray(int[]array)
+{
+    int Max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] > Max)
+         Max = array[i];
+    }
+    int Min = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] < Min)
+         Min = array[i];
+    }
+Console.WriteLine(Max - Min);
+}
+
+Console.Clear();
+Console.Write("Напишите необходимое количество цифр в массиве:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
+ReleaseArray(array);
