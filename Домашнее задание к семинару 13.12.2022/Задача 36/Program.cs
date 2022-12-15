7d8e5c83d437e65077961d918f6a4823dafd4f5a
«Ramil <Ramil1995@bk.ru>
2022-12-15 19:17:32 +0300

    Задача36

---
+++ Домашнее задание к семинару 13.12.2022/Задача 36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(-100, 101);
+}
+
+void ReleaseArray(int[]array)
+{
+    int SummEl = 0;
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        SummEl = SummEl + array[i];
+    }
+    Console.WriteLine(SummEl);
+}
+
+Console.Clear();
+Console.Write("Напишите необходимое количество цифр в массиве:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
