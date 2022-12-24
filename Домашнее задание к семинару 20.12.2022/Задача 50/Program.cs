d4b854404a79ffdd5a8fca18ce521e005ad1b76d
«Ramil <Ramil1995@bk.ru>
2022-12-24 23:47:52 +0300

    ПлощадьТреугольника

---
+++ Домашнее задание к семинару 20.12.2022/Задача 50/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции
+//  элемента в двумерном массиве, и возвращает значение этого элемента 
+//  или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1 7 -> такой позиции в массиве нет (пользователь вводит нумерацию с 1)
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    { 
+        for (int j = 1; j < matrix.GetLength(1); j++)
+        {
+            if ( i > matrix.GetLength(0) + 1 || j > matrix.GetLength(1) + 1);
+        }    
+    }
+    Console.Write("Такой позиции нет");
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.Write("Введите позицию массива: ");
+int[] mas = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix1 = new int[size[0], size[1]];
+ReleaseMatrix(matrix);
---
+++ Домашнее задание к семинару 20.12.2022/Площадь треугольника/Program.cs
@@ -0,0 +1,18 @@
+﻿// По целочисленным координатам вершин треугольника (x1,y1), (x2,y2) и (x3,y3) требуется вычислить его площадь.
+
+// Входные данные
+// Входной файл INPUT.TXT содержит 6 целых чисел x1,y1,x2,y2,x3,y3 – координаты вершин треугольника. Все числа не превышают 
+// 10 в 6 степени по абсолютной величине.
+
+// Выходные данные
+// В выходной файл OUTPUT.TXT выведите точное значение площади заданного треугольника.
+
+Console.Clear();
+double[] coord = Console.ReadLine().Split(" ").Select(x => Convert.ToDouble(x)).ToArray();
+double x1 = coord[0], y1 = coord[1], x2 = coord[2], y2 = coord[3], x3 = coord[4], y3 = coord[5];
+double AB = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+double BC = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+double AC = Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2));
+double p = (AB + BC + AC) / 2;
+double S = Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+Console.WriteLine(Math.Round(S, 2));
