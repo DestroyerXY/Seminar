2567cb051d813e38fd7ed070b5f4f0311281b795
«Ramil <Ramil1995@bk.ru>
2022-12-30 00:54:04 +0300

    Задача62

---
+++ Домашнее задание к семинару 8/Задача 62/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void PrintArray(int[,] spiral)
+{
+    for (int i = 0; i < spiral.GetLength(0); i++)
+    {
+        for (int j = 0; j < spiral.GetLength(1); j++)
+        {
+            Console.Write(spiral[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] spiral = new int[size[0], size[1]];
+int start = 1;
+int i = 0;
+int j = 0;
+
+while (start <= size[0] * size[1])
+{
+    spiral[i, j] = start;
+    if (i <= j + 1 && i + j < size[0] - 1)
+        j++;
+    else if (i < j && i + j >= size[0] - 1)
+        i++;
+    else if (i >= j && i + j > size[0] - 1)
+        j--;
+    else
+        i--;
+    start++;
+}
+PrintArray(spiral);  //Списал вслепую, не понял вообще. Нужно разобрать на примере.
