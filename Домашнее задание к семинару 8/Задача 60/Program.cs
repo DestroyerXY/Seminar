163829c884766178d77ecbdb84bae97dd81e7966
«Ramil <Ramil1995@bk.ru>
2022-12-30 00:49:19 +0300

    Задача60

---
+++ Домашнее задание к семинару 8/Задача 60/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void InputMatrix(int[,,] matrix)
+{
+    int number = 10;  //Двузначные числа
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = number++;
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+                Console.Write($"{matrix[i, j, z]} ({i}, {j}, {z}) \t"); //Добавление индекса каждого элемента
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите размер трехмерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] * size[1] * size[2] > 90)
+{
+    Console.WriteLine("Вы ошиблись.\nВведите заново размер трехмерного массива:");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,,] matrix = new int[size[0], size[1], size[2]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
